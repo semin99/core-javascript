babdc76e372628e51b212d37490acb0955c72489
semin99 <jsm7494@naver.com>
2023-01-05 10:27:11 +0900

    loop

---
+++ client/chapter/core/09-4.loop.js
@@ -0,0 +1,55 @@
+/* ---------------------------------------------------------------------- */
+/* For In Loop                                                            */
+/* ---------------------------------------------------------------------- */
+
+
+
+const javaScript = {
+  creator: 'Brendan Eich',
+  createAt: '1995.05',
+  standardName: 'ECMAScript',
+  currentVersion: 2022,
+};
+
+let key = "toString";
+
+// 객체의 속성(property) 포함 여부 확인 방법
+// - 모든 객체가 사용 가능하도록 속성이 확장되었을 때 포함 여부 결과는?
+
+
+// 객체 자신의 속성인지 확인하는 방법
+// - "자신의 속성을 가지고있는지 확인 방법"이 덮어쓰여질 수 있는 위험에 대처하는 안전한 방법은?
+
+console.log(Object.prototype.hasOwnProperty.call(javaScript, key));
+
+let a ={}
+
+for(let key in javaScript){
+  if(Object.prototype.hasOwnProperty.call(javaScript, key)){
+    console.log(key)
+  }
+}
+
+const user = {
+  name: 'semin',
+  age : 24
+}
+
+function hello(a,b,c){
+  console.log(this);
+}
+
+hello.call(user,1,2,3);
+
+// for ~ in 문
+// - 객체 자신의 속성만 순환하려면?
+// - 배열 객체 순환에 사용할 경우?
+
+let tens = [10,100,1000,10000];
+
+
+for(let value in tens){
+  if( ({}).hasOwnProperty.call(tens,value)){
+    console.log(tens[value]);
+  }
+}
