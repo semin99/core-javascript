9eeff6344ce2649b18683fcce56bee92e31fc901
semin99 <jsm7494@naver.com>
2023-01-19 15:07:08 +0900

    [추가] 유저카드랜더링 완성

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -1,5 +1,9 @@
 
 
+export * from './xhr.js'
 export * from './copy.js'
+export * from './delay.js'
 export * from './memo.js'
 export * from './typeOf.js'
+export * from './tiger.js'
+export * from './thema.js'
--- client/main.js
+++ client/main.js
@@ -1,1 +1,70 @@
-console.log(window);
+
+/* global gsap */
+
+
+import { 
+  getNode as $,
+  insertLast,
+  tiger,
+  delayP,
+  renderSpinner,
+  renderUserCard,
+  renderEmptyCard,
+  changeColor,
+  attr,
+  clearContents
+} from "./lib/index.js";
+
+
+const userCardContainer = $('.user-card-inner')
+
+async function redingUserList(){
+
+  renderSpinner(userCardContainer);
+
+  try{
+    await delayP(2000);
+    
+    $('.loadingSpinner').remove();
+
+    let response = await tiger.get('http://localhost:3000/users')
+  
+    let userDate = response.data;
+  
+    userDate.forEach(data => renderUserCard(userCardContainer, data));
+  
+    changeColor('.user-card');
+  
+    gsap.to(gsap.utils.toArray('.user-card'),{
+      x:0,
+      opacity:1,
+      duration:1.5,
+      stagger:0.2,
+    })
+  }catch(err){
+    renderEmptyCard(userCardContainer)
+  }
+
+}
+
+function handler(e){
+  let deleteButton = e.target.closest('button');
+  let article = e.target.closest('article');
+
+  if(!deleteButton || !article) return;
+
+  let id = attr(article, 'data-index').slice(5);
+
+  tiger.delete(`http://localhost:3000/users/${id}`).then(()=>{
+    clearContents(userCardContainer);
+    redingUserList();
+  })
+  
+}
+
+userCardContainer.addEventListener("click", handler)
+
+redingUserList();
+
+
+
