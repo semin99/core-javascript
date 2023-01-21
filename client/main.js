d9973e9bc89d4b4a5287c2c107bc31e641b0ad97
semin99 <jsm7494@naver.com>
2023-01-21 13:27:44 +0900

    storage 완성

--- client/main.js
+++ client/main.js
@@ -1,1 +1,33 @@
-console.log(window);
+
+
+
+/* import { 
+  getNode, loadStorage, saveStorage, deleteStorage
+} from "./lib/index.js";
+
+const textField = getNode('#textField')
+const deleteButton = getNode('input[value="삭제"]')
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+function inputHandler(){
+  saveStorage('area', textField.value)
+}
+
+function inputDeleteHandler(){
+  deleteStorage('area')
+}
+
+textField.addEventListener('input', inputHandler)
+deleteButton.addEventListener('click', inputDeleteHandler) */
+
+let n=1;
+
+function solution(n) {
+  let answer = [];
+  answer.map((acc, i) => i+1)
+}
+
+solution();
